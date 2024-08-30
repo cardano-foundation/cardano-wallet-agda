23731e6bbe58a0406bb21ee7e92c136dd1468473
paolino <paolo.veronelli@gmail.com>
2024-08-30 14:12:43 +0000

    Add stylish-haskell, hlint and hls CI steps

---
+++ scripts/buildkite/main/check-code-format.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Force UTF-8 encoding:
+LANG=C.UTF-8
+
+echo "+++ Check code format: imports"
+
+cd lib/cardano-wallet-read
+
+# shellcheck disable=SC2046
+stylish-haskell -v --config ../../.stylish-haskell.yaml --inplace $(git ls-files -- '*.hs')
+
+echo "+++ Check code format: no multiple consecutive blank lines"
+
+find . -type f -name '*.hs' -exec sed -i -r '/^$/N;/^\n$/D' {} +
+
+echo "+++ Check code format: no blank lines at the starts of files"
+
+find . -type f -name '*.hs' -exec sed -i -r '/./,$!d' {} +
+
+echo "+++ Check code format: no blank lines at the ends of files"
+
+find . -type f -name '*.hs' -exec sed -i -r -e :a -e '/^\n*$/{$d;N;ba' -e '}' {} +
+
+echo "+++ Check code format: no trailing whitespace"
+
+find . -type f -name '*.hs' -exec sed -i -r 's/[[:space:]]*$//' {} +
+
+git diff --exit-code
