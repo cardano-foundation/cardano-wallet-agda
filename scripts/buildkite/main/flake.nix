ed0095ff2ab0cf3ee43dc61e9af0f026a5a51aff
paolino <paolo.veronelli@gmail.com>
2024-09-02 14:29:04 +0000

    Add main pipeline nix.flake

---
+++ scripts/buildkite/main/flake.nix
@@ -0,0 +1,54 @@
+{
+  description = ''
+    Shell for the main pipeline
+    that builds release artifacts and runs E2E tests.
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:hamishmack/flake-utils/hkm/nested-hydraJobs";
+    attic.url = "github:zhaofengli/attic";
+  };
+
+  outputs = inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+       ]; in
+    inputs.flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        # Imports
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+        attic = inputs.attic.packages.${system}.default;
+      in {
+        packages = { };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.curl
+            pkgs.jq
+            pkgs.rsync
+            pkgs.gnutar
+            pkgs.gnupg
+
+            # Restore benchmark
+            pkgs.buildkite-agent
+            pkgs.gawk
+            pkgs.gnuplot
+            pkgs.gnugrep
+            pkgs.haskellPackages.hp2pretty
+            pkgs.time
+
+            # Nix Caching
+            attic
+          ];
+          shellHook = ''
+            # use this hook to set up additional environment variables
+          '';
+        };
+      }
+    );
+}
